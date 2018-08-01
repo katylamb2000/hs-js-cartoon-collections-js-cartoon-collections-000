efba3795fbcf0543b1b1a3743fa97121847764bd
katy@karstaway.com <katy@karstaway.com>
2018-08-01 15:45:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+  
+
 function dwarfRollCall(dwarves) {
+var array = [];
+  for (let i=0; i<dwarves.length; i++){
+  array.push (`${i+1}. ${dwarves[i]} `)
+  
+  }
+  return array.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  for (let i = 0; i<planeteerCalls.length; i++) {
+    array.push `${planeteerCalls[i].toUpperCase}! `
+  }
+  return array
 }
 
 function longPlaneteerCalls(words) {
