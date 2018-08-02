55eb3b3b0a535b4424aacd05b217d904fe19da4e
katy@karstaway.com <katy@karstaway.com>
2018-08-02 00:05:28 +0000

    Done.

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ var array = [];
   
   }
   return array.join("")
+
+
 }
 
 function summonCaptainPlanet(planeteerCalls){
   var array = []
   for (let i = 0; i<planeteerCalls.length; i++) {
-    array.push `${planeteerCalls[i].toUpperCase}! `
+    array.push (`${planeteerCalls[i].toUpperCase}! `)
   }
   return array
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i<words.length; i++) {
+  if (words[i].length <5) 
+    return false
+    else return true
+  }
 }
 
 function findTheCheese (foods) {
